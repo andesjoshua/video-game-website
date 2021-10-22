745bfcb56daaddf3501897e489b79d45997a352b
Joshua Andes <andesjoshua@comcast.net>
2021-10-22 18:23:50 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+const [gameData, setGameData] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.rawg.io/api/platforms?key=34a81b8193a846b5aba89c9356f750ec')
+    .then(r => r.json())
+    .then(data => console.log(data))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
