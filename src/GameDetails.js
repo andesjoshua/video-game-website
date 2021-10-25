0cad1431983a1889bd32ce77267764432d95694f
Joshua Andes <andesjoshua@comcast.net>
2021-10-25 11:32:12 -0400

    added-button-for-details

--- src/GameDetails.js
+++ src/GameDetails.js
@@ -1,13 +1,7 @@
 
 const GameDetails = ({details: {genre, platform, publisher, release_date, short_description}, details}) => {
 
-    const generateLi = (details) => {
-        details.map(gameDetail => {
-            for(let i = 0; i < gameDetail.lenth; i++) {
-                console.log(gameDetail[i])
-            }
-        })
-    }
+   
     return (
         <div>
             <li>Genre: {genre}</li>
